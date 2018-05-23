7972359d0a04d367f6db89c071d769830c58c602
Hopson97 <mhopson@hotmail.co.uk>
2018-05-23 12:27:46 +0100

    begin iterating over the files in cwd

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,26 @@
+#include <unordered_set>
 #include <iostream>
+#include "Filesystem.h"
+
+void iterateFiles(const fs::path& directory)
+{
+    for (auto& entry : fs::directory_iterator(directory)) {
+        const auto& path = entry.path();
+    }
+}
 
 int main()
 {
-    std::cout << "Hello world\n";
+    std::unordered_set<std::string> sourceFiles;
+    std::unordered_set<std::string> headerFiles;
+    fs::directory_iterator itr(fs::current_path());
+
+    iterateFiles(fs::current_path());
+
+
+
+
+
+
+    std::cin.ignore();
 }
