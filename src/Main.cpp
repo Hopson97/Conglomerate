8455c0edb26a5a45652f34964cddcbc0321243b5
Hopson97 <mhopson@hotmail.co.uk>
2018-05-23 14:52:06 +0100

    begin parsing source files

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,32 +1,55 @@
-#include <unordered_set>
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_set>
+#include <regex>
+#include <fstream>
+#include <cstdlib>
 #include "Filesystem.h"
 
-void iterateFiles(const fs::path& directory)
+bool startsWith(const std::string& string, const std::string& prefix) 
+{
+    return !std::strncmp(string.c_str(), prefix.c_str(), prefix.length());
+}
+
+void findSourceFiles(const fs::path& directory, std::vector<fs::path>& sources)
 {
     for (auto& entry : fs::directory_iterator(directory)) {
         const auto& path = entry.path();
-        if (path.extension() == ".cpp" || path.extension() == ".h") {
-            std::cout << path << "\n";
+        if (path.extension() == ".cpp" || path.extension() == ".c") {
+            sources.push_back(path);
         }
         else if (fs::is_directory(path)) {
-            iterateFiles(path);
+            findSourceFiles(path, sources);
         }
     }
 }
 
-int main()
+void findHeaderFiles(fs::path& file, std::unordered_set<std::string>& headers)
 {
-    std::unordered_set<std::string> sourceFiles;
-    std::unordered_set<std::string> headerFiles;
-    fs::directory_iterator itr(fs::current_path());
+    std::cout << "\nLooking for header files in " << file << "\n";
+    std::ifstream inFile(file);
+    std::string line;
+    while (std::getline(inFile, line)) {
+        if (startsWith(line, "#include")) {
+            line.erase(0, 9); //remove the #include 
+            line.erase(line.length() - 1, line.length() - 1);
+            std::cout << line << "\n";
+        }
+    }
+    
+}
 
-    iterateFiles(fs::current_path());
+int main(int argc, char** argv)
+{
+    std::vector<fs::path> sources;
+    std::unordered_set<std::string> headers;
 
+    findSourceFiles(fs::current_path(), sources);
 
-
-
-
-
-    std::cin.ignore();
+    std::cout << "Number of source files found: " << sources.size() << "\n";
+    std::cout << "Finding header files...\n";
+    for (auto& sourceFile : sources) {
+        findHeaderFiles(sourceFile, headers);
+    }
 }
